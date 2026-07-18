bb74a64d12fbed5d7788be04bc5828cac4925334
Amartya Saran <amartyasaran@gmail.com>
2026-07-18 23:22:13 +0530

    RAG/step07/scaffold fixed

--- backend/app/projects/RAG/step07/scaffold.py
+++ backend/app/projects/RAG/step07/scaffold.py
@@ -1,5 +1,6 @@
 from sklearn.metrics.pairwise import cosine_similarity
-from llama_index.embeddings.huggingface import HuggingFaceEmbeddingfrom sklearn.metrics.pairwise import cosine_similarity
+from llama_index.embeddings.huggingface import HuggingFaceEmbedding
+from sklearn.metrics.pairwise import cosine_similarity
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 
 {{USER_CODE}}
